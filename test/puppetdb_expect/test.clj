13ebf56c39ffffac46833eb59045973738238583
Gareth Rushgrove <gareth@morethanseven.net>
2014-08-25 16:17:32 +0100

    cleanup and correct tests

--- test/puppetdb_expect/test.clj
+++ test/puppetdb_expect/test.clj
@@ -3,31 +3,35 @@
             [expectations :refer [expect from-each]]
             [environ.core :refer [env]]))
 
-
 (def client (puppetdb/connect (env :puppetdb-address)))
 
 (def nodes (puppetdb/query client "/v3/nodes"))
 
+(def average-resources-per-node
+  (:Value (puppetdb/query client "/v3/metrics/mbean/com.puppetlabs.puppetdb.query.population:type=default,name=avg-resources-per-node")))
+
 (defn installed? [node package]
-  (= false (empty? (puppetdb/query client (str "/v3/nodes/" node "/resources/Package/" package)))))
+  (= false (empty?
+    (puppetdb/query client (str "/v3/nodes/" node "/resources/Package/" package)))))
+
+(defn ubuntu? [value] (= "Ubuntu" value))
+
+(defn installed-everywhere? [package]
+  (= (count nodes)
+    (count (puppetdb/query client (str "/v3/resources/Package/" package)))))
 
 ; check number of nodes
-(expect 2 (count nodes))
+(expect 1 (count nodes))
 
 ; check all nodes have wget installed
-(expect (complement empty?) (from-each
-  [node (puppetdb/query client "/v3/resources/Package/wget")] node))
+(expect installed-everywhere? "wget")
 
 ; check specific node has package installed
 (expect (installed? "master" "pe-puppetdb"))
 
-(defn ubuntu? [value] (= "Ubuntu" value))
-
 ; check all machines are running Ubuntu
-(expect empty? (remove ubuntu?
-    (map :value (puppetdb/query client "/v3/facts/operatingsystem"))))
+(expect (every? ubuntu?
+  (map :value (puppetdb/query client "/v3/facts/operatingsystem"))))
 
-; moving on from just checking counts, this checks the value of all returned
-; nodes that match the query
-(expect "Linux" (from-each
-  [node (puppetdb/query client "/v3/facts/kernel")] (node :value)))
+; keep a check on the number of resources
+(expect (< average-resources-per-node 300))
